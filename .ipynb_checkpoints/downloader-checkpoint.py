1c8a77e5814d476773459c6f2e06320d0c4ed659
Haneul Choi <caelum02@snu.ac.kr>
2020-09-24 17:57:01 +0900

    gitignore clean

--- .ipynb_checkpoints/downloader-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import gdown
-
-url = 'https://drive.google.com/file/d/1dPjH-QAHACJ747QCDOLf3iawNKyHCN1E/view?usp=sharing'
-id_ = url.split('/')[5]
-url = 'https://drive.google.com/uc?id=' + id_
-gdown.download(url, quiet=False)
